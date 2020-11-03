cdac50ed8acd7dcdbf978a465473fdb834997294
LuJK <1454446059@qq.com>
2020-11-03 10:35:14 +0800

    开发

--- src/network/request.js
+++ src/network/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 export function request(config) {
      const instance = axios.create({
-        baseURL: 'http://106.54.54.237:8000/xxt/ah8',
+        baseURL: 'http://152.136.185.210:8000/api/w6',
         timeout: 8000
     })
     instance.interceptors.request.use(config => {
